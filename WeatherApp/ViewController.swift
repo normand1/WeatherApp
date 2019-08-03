71333f3d6fc719dd2eb620452cbd1add6140de18
David Norman <david.norman.w@gmail.com>
2019-08-02 17:52:35 -0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by David Norman on 8/2/19.
+//  Copyright © 2019 David Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
